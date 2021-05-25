4390686c1d5009505c8ad74b4ef5ad03e33f9fab
Moritz Miedler <miedler.m03@htlwienwest.at>
2021-05-25 11:37:51 +0200

    erstellt

---
+++ sai-product-website/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="SAI Logo"
+          class="shrink mr-2"
+          contain
+          src="/assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="GAMEURL"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Play the game</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
